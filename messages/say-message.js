e1a8a3f4485a7a3f281df7d7aa7aa965c5f2ef75
Nic <57350249+python-admin@users.noreply.github.com>
2022-07-28 10:46:07 +0800

    done

--- messages/say-message.js
+++ messages/say-message.js
@@ -1,7 +1,9 @@
-
+const needed = require("./hello-message");
+const helloMessage = needed.helloMessage;
 
 function sayMessage(message) {
   console.log(`"${message}"`)
 }
 
 sayMessage(helloMessage);
+exports.sayMessage = sayMessage;
